3078d5888c3b23336d8c6cf9f5517bd08f125039
Dyadyushka Maxim <donchik125@gmail.com>
2014-10-10 10:24:17 +0300

    Initial commit

--- src/test/java/com/academysmart/repositorty/EmployeeRepositorySingletonTest.java
+++ src/test/java/com/academysmart/repositorty/EmployeeRepositorySingletonTest.java
@@ -1,6 +1,8 @@
 package com.academysmart.repositorty;
 
 import com.academysmart.exception.IncorrectEmailException;
+import com.academysmart.exception.ServletException;
+import com.academysmart.repository.EmployeeRepositorySingleton;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -9,19 +11,20 @@ public class EmployeeRepositorySingletonTest {
 
 	@BeforeClass
 	public static void beforeClass()throws Exception {
-//		EmployeeRepositorySingleton.getRepository().addEmployee("����",
-//				"������", "ivanov@mail.ru");
-//	}
+		EmployeeRepositorySingleton.getRepository().addEmployee("ivan",
+				"ivanov", "ivanov@mail.ru");
+	}
 
-//	@Test
-//	public void testGetRepositoryReturnOneInstance() {
-//		Assert.fail("Test is not implemented");
-//	}
-//
-//
-//	@Test(expected=IncorrectEmailException.class)
-//	public void testAddEmployeWithIncorrectEmail() throws IncorrectEmailException  {
-//		Assert.fail("Test is not implemented");
+	@Test
+	public void testGetRepositoryReturnOneInstance() {
+        EmployeeRepositorySingleton first = EmployeeRepositorySingleton.getRepository();
+        EmployeeRepositorySingleton second = EmployeeRepositorySingleton.getRepository();
+		Assert.assertEquals(first, second);
+	}
+
+	@Test(expected=IncorrectEmailException.class)
+	public void testAddEmployeWithIncorrectEmail() throws ServletException {
+		Assert.fail(String.valueOf(EmployeeRepositorySingleton.getRepository().addEmployee("name", "lname", "ivanov@mail.ru")));
 	}
 
 }
